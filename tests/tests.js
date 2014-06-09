dbb165b4120e86bf4f3f982f3ce2394535d4d77d
Rickard Karlsson <sunnre3@gmail.com>
2014-06-09 07:02:49 +0200

    added tests for gateway and price entries

--- tests/tests.js
+++ tests/tests.js
@@ -25,7 +25,7 @@ test('Valid config - username and password provided', function () {
 	ok(true);
 });
 test('Invalid config - username but no password provided', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
 			'username': 'username'
 		};
@@ -34,7 +34,7 @@ test('Invalid config - username but no password provided', function () {
 	}, /password/);
 });
 test('Invalid config - password but no username provided', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
 			'password': 'password'
 		};
@@ -43,7 +43,7 @@ test('Invalid config - password but no username provided', function () {
 	}, /username/);
 });
 test('Invalid config - password is blankspace', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
 			'username': 'username',
 			'password': '    '
@@ -53,7 +53,7 @@ test('Invalid config - password is blankspace', function () {
 	}, /password/);
 });
 test('Invalid config - username is blankspace', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
 			'username': '    ',
 			'password': 'password'
@@ -63,7 +63,7 @@ test('Invalid config - username is blankspace', function () {
 	}, /username/);
 });
 test('Invalid config - username and password provided but http omitted', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
 			'username': 'username',
 			'password': 'password'
@@ -73,10 +73,10 @@ test('Invalid config - username and password provided but http omitted', functio
 	}, /you need to inject a http/i);
 });
 test('Invalid config - username and password provided but http is not the correct class.', function () {
-	throws(function() {
+	throws(function () {
 		var config = {
-			'username': 'username',
-			'password': 'password',
+			'username': 'test',
+			'password': 'test',
 			'http': new String()
 		};
 
@@ -84,13 +84,13 @@ test('Invalid config - username and password provided but http is not the correc
 	}, /is not an instance of/i);
 })
 
-module('clx.http - request tests');
+module('clx.api - operator request tests');
 asyncTest('Valid request - getOperators()', function () {
 	expect(1);
 
 	var config = {
-		'username': 'username',
-		'password': 'password',
+		'username': ' rest_api',
+		'password': ' rest_api',
 		'http': new clx.httpTest()
 	};
 	var obj = new clx.api(config);
@@ -104,8 +104,8 @@ asyncTest('Valid request - getOperatorById()', function () {
 	expect(1)
 
 	var config = {
-		'username': 'username',
-		'password': 'password',
+		'username': ' rest_api',
+		'password': ' rest_api',
 		'http': new clx.httpTest()
 	};
 	var obj = new clx.api(config);
@@ -115,15 +115,265 @@ asyncTest('Valid request - getOperatorById()', function () {
 		start();
 	});
 });
-test('Invalid request - getOperatorById(): string passed as operator id', function() {
-	throws(function() {
+test('Invalid request - getOperatorById(): string passed as operator id', function () {
+	throws(function () {
 		var config = {
-			'username': 'username',
-			'password': 'password',
+			'username': 'test',
+			'password': 'test',
 			'http': new clx.httpTest()
 		};
 		var obj = new clx.api(config);
 
 		obj.getOperatorById('string');
+	}, /parameter 'operatorId' must be an integer/);
+});
+test('Invalid request - getOperatorById(): operator id omitted', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getOperatorById();
+	}, /parameter 'operatorId' must be an integer/);
+});
+
+module('clx.api - gateway request tests');
+asyncTest('Valid request - getGateways()', function () {
+	expect(1);
+
+	var config = {
+		'username': ' rest_api',
+		'password': ' rest_api',
+		'http': new clx.httpTest()
+	};
+	var obj = new clx.api(config);
+
+	obj.getGateways(function() {
+		equal(obj.getStatusCode(), 200);
+		start();
+	});
+});
+asyncTest('Valid request - getGatewayById()', function () {
+	expect(1)
+
+	var config = {
+		'username': ' rest_api',
+		'password': ' rest_api',
+		'http': new clx.httpTest()
+	};
+	var obj = new clx.api(config);
+
+	obj.getGatewayById(2186, function() {
+		equal(obj.getStatusCode(), 200);
+		start();
+	});
+});
+test('Invalid request - getGatewayById(): string passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById('string');
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getGatewayById(): null passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById(null, function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getGatewayById(): gateway id omitted', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById();
+	}, /parameter 'gatewayId' must be an integer/);
+});
+
+module('clx.api - price entries request tests');
+asyncTest('Valid request - getPriceEntriesByGatewayId()', function () {
+	expect(1)
+
+	var config = {
+		'username': ' rest_api',
+		'password': ' rest_api',
+		'http': new clx.httpTest()
+	};
+	var obj = new clx.api(config);
+
+	obj.getGatewayById(2186, function() {
+		equal(obj.getStatusCode(), 200);
+		start();
+	});
+});
+test('Invalid request - getPriceEntriesByGatewayId(): string passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById('string', function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayId(): null passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById(null, function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayId(): null passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById(null);
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayId(): gateway id omitted', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getGatewayById();
+	}, /parameter 'gatewayId' must be an integer/);
+});
+asyncTest('Valid request - getPriceEntriesByGatewayIdAndOperatorId()', function () {
+	expect(1)
+
+	var config = {
+		'username': ' rest_api',
+		'password': ' rest_api',
+		'http': new clx.httpTest()
+	};
+	var obj = new clx.api(config);
+
+	obj.getPriceEntriesByGatewayIdAndOperatorId(2186, 10, function() {
+		equal(obj.getStatusCode(), 200);
+		start();
+	});
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): string passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId('string', 10, function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): null passed as gateway id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId(null, 10, function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): gateway id omitted', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId(undefined, 10, function () {
+			// Empty callback.
+		});
+	}, /parameter 'gatewayId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): string passed as operator id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId(2186, 'string', function () {
+			// Empty callback.
+		});
+	}, /parameter 'operatorId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): null passed as operator id', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId(2186, null, function () {
+			// Empty callback.
+		});
+	}, /parameter 'operatorId' must be an integer/);
+});
+test('Invalid request - getPriceEntriesByGatewayIdAndOperatorId(): operator id omitted', function () {
+	throws(function () {
+		var config = {
+			'username': 'test',
+			'password': 'test',
+			'http': new clx.httpTest()
+		};
+		var obj = new clx.api(config);
+
+		obj.getPriceEntriesByGatewayIdAndOperatorId(2186, undefined, function () {
+			// Empty callback.
+		});
 	}, /parameter 'operatorId' must be an integer/);
 });
